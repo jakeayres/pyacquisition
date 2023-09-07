e2def0b61bdda3b1e1e11962db12457873d97dfa
jakeayres <jake_ayres@hotmail.co.uk>
2023-09-07 18:44:50 +0100

    Update dummy.py
    Dummy GPIB backend returns random numbers to all queries and 0 to all commands

--- src/pyacquisition/visa/dummy.py
+++ src/pyacquisition/visa/dummy.py
@@ -1,8 +1,38 @@
+import numpy as np
+
+
+class DummyResourceManager:
+
+
+	def __init__(self, output:str='random'):
+		self._output = output
+
+
+	def _write(self, command):
+		pass
+
+
+	def open_resource(self, gpib_resource_string, *args, **kwargs):
+		return DummyResource(*args, output=self._output, **kwargs)
 
 
 
-class DummyResourceManager(object):
+class DummyResource:
 
 
-	def open_resource(self, gpib_resource_string):
+	def __init__(self, 
+		read_termination=None,
+		write_termination=None,
+		output:str='random'
+		):
+		self._output = output
+
+
+	def write(self, command):
 		return 0
+
+
+	def query(self, command):
+
+		if self._output == 'random':
+			return f'{np.random.random()*10:.3f}'
